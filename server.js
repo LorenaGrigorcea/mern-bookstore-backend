54f41cee67d4905bfb90e6705cb0877aa77f9a1a
Lorena Grigorcea <lorenagrigorcea@gmail.com>
2025-12-03 18:52:05 +0200

    Server.js update

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credential: true
+}));
 app.use(express.json());
 
 const PRODUCTS_FILE = path.join(__dirname, 'data', 'books.json');
